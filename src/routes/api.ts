abe8b9bc3e8848b4f5424a5d1fe1a67b85150717
Ishizuka13 <miguelishizuka4@gmail.com>
2023-03-24 10:25:18 -0300

    .

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
-
+import { Auth } from '../middlewares/auth';
 import * as ApiController from '../controllers/apiController';
 
 const router = Router();
 
-router.get('/ping', ApiController.ping);
-
 router.post('/register', ApiController.register);
 router.post('/login', ApiController.login);
 
-router.get('/list', ApiController.list);
+router.get('/list', Auth.private, ApiController.list);
 
 export default router;
